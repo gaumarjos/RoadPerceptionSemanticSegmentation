ba57b1c02f20cc81de65966cee6f38711e3fe7ef
Alexey <alexey.simonov@gmail.com>
2017-09-08 16:00:15 +0100

    minor fix of var name

--- main.py
+++ main.py
@@ -123,7 +123,7 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     sess.run(tf.global_variables_initializer())
     for epoch in range(epochs):
         # running optimization in batches of training set
-        n_batches = int(math.ceil(float(n_samples) / batch_size))
+        n_batches = int(math.ceil(float(_n_samples) / batch_size))
         batches_pbar = tqdm(get_batches_fn(batch_size),
                             desc='Train Epoch {:>2}/{}'.format(epoch + 1, epochs),
                             unit='batches',
