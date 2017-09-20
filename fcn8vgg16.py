257298672031020d060c97288c1451ae8e4b32e5
Alexey <alexey.simonov@gmail.com>
2017-09-20 17:56:43 +0100

    work in progress. creating proper definition of the full FCN. loading pre-trained VGG weights

---
+++ fcn8vgg16.py
@@ -0,0 +1,306 @@
+import tensorflow as tf
+import math
+from tqdm import tqdm
+import os
+
+class FCN8_VGG16:
+    def __init__(self, images_shape, labels_shape):
+        self._images_shape = images_shape
+        self._labels_shape = labels_shape
+        self._keep_prob = tf.placeholder(tf.float32, name='keep_prob', shape=[])
+        self._learning_rate = tf.placeholder(tf.float32, name='lr', shape=[])
+
+        self._create_input_pipeline()
+        with tf.name_scope("encoder_vgg16"):
+            self._create_vgg16_conv_layers()
+            self._create_vgg16_fc_conv_layers()
+        self._create_decoder()
+        self._create_optimizer()
+        #if weights is not None and sess is not None:
+        #    self.load_weights(weights, sess)
+
+    def _create_optimizer(self):
+        num_classes = self._labels_shape[-1]
+        logits = tf.reshape(self._output, (-1, num_classes), name='logits')
+        # TODO: use weighted loss based on how our classes are represented?
+        self._loss = tf.reduce_mean(
+                                tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=self._labels_float),
+                                name="loss")
+        self._optimizer = tf.train.AdamOptimizer(self._learning_rate).minimize(self._loss)
+
+    def train(self, sess, epochs, batch_size, get_batches_fn, n_samples, keep_prob_value, learning_rate):
+        """
+        Train neural network and print out the loss during training.
+        :param sess: TF Session
+        :param epochs: Number of epochs
+        :param batch_size: Batch size
+        :param get_batches_fn: Function to get batches of training data.  Call using get_batches_fn(batch_size)
+        """
+        # save intermediate checkpoint during training
+        saver = tf.train.Saver()  # by default saves all variables
+        if not os.path.exists('ckpt2'):
+            os.makedirs('ckpt2')
+        checkpoint_dir = 'ckpt2/model.ckpt'
+        ckpt = tf.train.get_checkpoint_state(os.path.dirname(checkpoint_dir))
+        # if that checkpoint exists, restore from checkpoint
+        if ckpt and ckpt.model_checkpoint_path:
+            saver.restore(sess, ckpt.model_checkpoint_path)
+            print("restored from checkpoint {}".format(ckpt.model_checkpoint_path))
+
+        for epoch in range(epochs):
+            # running optimization in batches of training set
+            n_batches = int(math.ceil(float(n_samples) / batch_size))
+            batches_pbar = tqdm(get_batches_fn(batch_size, n_samples),
+                                desc='Train Epoch {:>2}/{} (loss _.___)'.format(epoch + 1, epochs),
+                                unit='batches',
+                                total=n_batches)
+            n = 0.
+            l = 0.
+            for images, labels in batches_pbar:
+                feed_dict = {self._images: images,
+                             self._labels: labels,
+                             self._keep_prob: keep_prob_value,
+                             self._learning_rate: learning_rate}
+                _, loss = sess.run([self._optimizer, self._loss],  # , self._summaries
+                                   feed_dict=feed_dict)
+                n += len(images)
+                l += loss * len(images)
+                batches_pbar.set_description(
+                    'Train Epoch {:>2}/{} (loss {:.3f})'.format(epoch + 1, epochs, l / n))
+                # write training summaries for tensorboard every so often
+                # step = self._global_step.eval(session=self._session)
+                # if step % 5 == 0:
+                #    summary_writer.add_summary(summaries, global_step=step)
+                # if i % 100 == 99:  # Record execution stats
+                #     run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+                #     run_metadata = tf.RunMetadata()
+                #     summary, _ = sess.run([merged, train_step],
+                #                           feed_dict=feed_dict(True),
+                #                           options=run_options,
+                #                           run_metadata=run_metadata)
+                #     train_writer.add_run_metadata(run_metadata, 'step%d' % i)
+                #     train_writer.add_summary(summary, i)
+                #     print('Adding run metadata for', i)
+                # else:  # Record a summary
+                #     summary, _ = sess.run([merged, train_step], feed_dict=feed_dict(True))
+                #     train_writer.add_summary(summary, i)
+
+            l /= _n_samples
+            # batches_pbar.set_description("loss over last epoch {}".format(l))
+
+            save_path = saver.save(sess, checkpoint_dir)  # , global_step=self._global_step)
+            # print("checkpoint saved to {}".format(save_path))
+        return l
+
+    def _create_decoder(self):
+        num_classes = self._labels_shape[-1]
+        with tf.name_scope("decoder"):
+            conv_1x1 = tf.layers.conv2d(self._conv7, num_classes, kernel_size=1,
+                                        strides=(1, 1), padding='SAME',
+                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
+                                        name='conv_1x1')
+            # in the paper 'initialise to bilinear interpolation'. here we do random initialization
+            up4 = tf.layers.conv2d_transpose(conv_1x1, 512,
+                                             kernel_size=4, strides=2, padding='SAME',
+                                             kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
+                                             name='up4')
+            skip4 = tf.add(up4, self._pool4, name='skip4')
+            up3 = tf.layers.conv2d_transpose(skip4, 256,
+                                             kernel_size=4, strides=2, padding='SAME',
+                                             kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
+                                             name='up3')
+            skip3 = tf.add(up3, self._pool3, name='skip3')
+            self._output = tf.layers.conv2d_transpose(skip3, num_classes,
+                                             kernel_size=16, strides=8, padding='SAME',
+                                             kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
+                                             name='output')
+
+    def _create_input_pipeline(self):
+        # define input placeholders in the graph
+        with tf.name_scope("data"):
+            self._images = tf.placeholder(tf.uint8, name='images', shape=self._images_shape)
+            tf.summary.image('input_images', self._images, 3)
+            self._labels = tf.placeholder(tf.uint8, name='labels', shape=self._labels_shape)
+        # zero-mean input
+        with tf.name_scope('preprocess') as scope:
+            self._images_float = tf.image.convert_image_dtype(self._images, tf.float32)
+            self._images_std = tf.map_fn(lambda img: tf.image.per_image_standardization(img), self._images_float)
+            self._labels_float = tf.cast(self._labels, tf.float32)
+
+    def _create_vgg16_conv_layers(self):
+        self._parameters = []
+
+        # conv1_1
+        with tf.name_scope('conv1_1') as scope:
+            kernel = tf.Variable(tf.truncated_normal([3, 3, 3, 64], dtype=tf.float32, stddev=1e-1), name='weights')
+            conv = tf.nn.conv2d(self._images_std, kernel, [1, 1, 1, 1], padding='SAME')
+            biases = tf.Variable(tf.constant(0.0, shape=[64], dtype=tf.float32), trainable=True, name='biases')
+            out = tf.nn.bias_add(conv, biases)
+            conv1_1 = tf.nn.relu(out, name=scope)
+            self._parameters += [kernel, biases]
+
+        # conv1_2
+        with tf.name_scope('conv1_2') as scope:
+            kernel = tf.Variable(tf.truncated_normal([3, 3, 64, 64], dtype=tf.float32, stddev=1e-1), name='weights')
+            conv = tf.nn.conv2d(conv1_1, kernel, [1, 1, 1, 1], padding='SAME')
+            biases = tf.Variable(tf.constant(0.0, shape=[64], dtype=tf.float32), trainable=True, name='biases')
+            out = tf.nn.bias_add(conv, biases)
+            conv1_2 = tf.nn.relu(out, name=scope)
+            self._parameters += [kernel, biases]
+
+        # pool1
+        pool1 = tf.nn.max_pool(conv1_2,
+                               ksize=[1, 2, 2, 1],
+                               strides=[1, 2, 2, 1],
+                               padding='SAME',
+                               name='pool1')
+
+        # conv2_1
+        with tf.name_scope('conv2_1') as scope:
+            kernel = tf.Variable(tf.truncated_normal([3, 3, 64, 128], dtype=tf.float32, stddev=1e-1), name='weights')
+            conv = tf.nn.conv2d(pool1, kernel, [1, 1, 1, 1], padding='SAME')
+            biases = tf.Variable(tf.constant(0.0, shape=[128], dtype=tf.float32), trainable=True, name='biases')
+            out = tf.nn.bias_add(conv, biases)
+            conv2_1 = tf.nn.relu(out, name=scope)
+            self._parameters += [kernel, biases]
+
+        # conv2_2
+        with tf.name_scope('conv2_2') as scope:
+            kernel = tf.Variable(tf.truncated_normal([3, 3, 128, 128], dtype=tf.float32, stddev=1e-1), name='weights')
+            conv = tf.nn.conv2d(conv2_1, kernel, [1, 1, 1, 1], padding='SAME')
+            biases = tf.Variable(tf.constant(0.0, shape=[128], dtype=tf.float32), trainable=True, name='biases')
+            out = tf.nn.bias_add(conv, biases)
+            conv2_2 = tf.nn.relu(out, name=scope)
+            self._parameters += [kernel, biases]
+
+        # pool2
+        pool2 = tf.nn.max_pool(conv2_2,
+                               ksize=[1, 2, 2, 1],
+                               strides=[1, 2, 2, 1],
+                               padding='SAME',
+                               name='pool2')
+
+        # conv3_1
+        with tf.name_scope('conv3_1') as scope:
+            kernel = tf.Variable(tf.truncated_normal([3, 3, 128, 256], dtype=tf.float32, stddev=1e-1), name='weights')
+            conv = tf.nn.conv2d(pool2, kernel, [1, 1, 1, 1], padding='SAME')
+            biases = tf.Variable(tf.constant(0.0, shape=[256], dtype=tf.float32), trainable=True, name='biases')
+            out = tf.nn.bias_add(conv, biases)
+            conv3_1 = tf.nn.relu(out, name=scope)
+            self._parameters += [kernel, biases]
+
+        # conv3_2
+        with tf.name_scope('conv3_2') as scope:
+            kernel = tf.Variable(tf.truncated_normal([3, 3, 256, 256], dtype=tf.float32, stddev=1e-1), name='weights')
+            conv = tf.nn.conv2d(conv3_1, kernel, [1, 1, 1, 1], padding='SAME')
+            biases = tf.Variable(tf.constant(0.0, shape=[256], dtype=tf.float32), trainable=True, name='biases')
+            out = tf.nn.bias_add(conv, biases)
+            conv3_2 = tf.nn.relu(out, name=scope)
+            self._parameters += [kernel, biases]
+
+        # conv3_3
+        with tf.name_scope('conv3_3') as scope:
+            kernel = tf.Variable(tf.truncated_normal([3, 3, 256, 256], dtype=tf.float32, stddev=1e-1), name='weights')
+            conv = tf.nn.conv2d(conv3_2, kernel, [1, 1, 1, 1], padding='SAME')
+            biases = tf.Variable(tf.constant(0.0, shape=[256], dtype=tf.float32), trainable=True, name='biases')
+            out = tf.nn.bias_add(conv, biases)
+            conv3_3 = tf.nn.relu(out, name=scope)
+            self._parameters += [kernel, biases]
+
+        # pool3
+        self._pool3 = tf.nn.max_pool(conv3_3,
+                               ksize=[1, 2, 2, 1],
+                               strides=[1, 2, 2, 1],
+                               padding='SAME',
+                               name='pool3')
+
+        # conv4_1
+        with tf.name_scope('conv4_1') as scope:
+            kernel = tf.Variable(tf.truncated_normal([3, 3, 256, 512], dtype=tf.float32, stddev=1e-1), name='weights')
+            conv = tf.nn.conv2d(self._pool3, kernel, [1, 1, 1, 1], padding='SAME')
+            biases = tf.Variable(tf.constant(0.0, shape=[512], dtype=tf.float32), trainable=True, name='biases')
+            out = tf.nn.bias_add(conv, biases)
+            conv4_1 = tf.nn.relu(out, name=scope)
+            self._parameters += [kernel, biases]
+
+        # conv4_2
+        with tf.name_scope('conv4_2') as scope:
+            kernel = tf.Variable(tf.truncated_normal([3, 3, 512, 512], dtype=tf.float32, stddev=1e-1), name='weights')
+            conv = tf.nn.conv2d(conv4_1, kernel, [1, 1, 1, 1], padding='SAME')
+            biases = tf.Variable(tf.constant(0.0, shape=[512], dtype=tf.float32), trainable=True, name='biases')
+            out = tf.nn.bias_add(conv, biases)
+            conv4_2 = tf.nn.relu(out, name=scope)
+            self._parameters += [kernel, biases]
+
+        # conv4_3
+        with tf.name_scope('conv4_3') as scope:
+            kernel = tf.Variable(tf.truncated_normal([3, 3, 512, 512], dtype=tf.float32, stddev=1e-1), name='weights')
+            conv = tf.nn.conv2d(conv4_2, kernel, [1, 1, 1, 1], padding='SAME')
+            biases = tf.Variable(tf.constant(0.0, shape=[512], dtype=tf.float32), trainable=True, name='biases')
+            out = tf.nn.bias_add(conv, biases)
+            conv4_3 = tf.nn.relu(out, name=scope)
+            self._parameters += [kernel, biases]
+
+        # pool4
+        self._pool4 = tf.nn.max_pool(conv4_3,
+                               ksize=[1, 2, 2, 1],
+                               strides=[1, 2, 2, 1],
+                               padding='SAME',
+                               name='pool4')
+
+        # conv5_1
+        with tf.name_scope('conv5_1') as scope:
+            kernel = tf.Variable(tf.truncated_normal([3, 3, 512, 512], dtype=tf.float32, stddev=1e-1), name='weights')
+            conv = tf.nn.conv2d(self._pool4, kernel, [1, 1, 1, 1], padding='SAME')
+            biases = tf.Variable(tf.constant(0.0, shape=[512], dtype=tf.float32), trainable=True, name='biases')
+            out = tf.nn.bias_add(conv, biases)
+            conv5_1 = tf.nn.relu(out, name=scope)
+            self._parameters += [kernel, biases]
+
+        # conv5_2
+        with tf.name_scope('conv5_2') as scope:
+            kernel = tf.Variable(tf.truncated_normal([3, 3, 512, 512], dtype=tf.float32, stddev=1e-1), name='weights')
+            conv = tf.nn.conv2d(conv5_1, kernel, [1, 1, 1, 1], padding='SAME')
+            biases = tf.Variable(tf.constant(0.0, shape=[512], dtype=tf.float32), trainable=True, name='biases')
+            out = tf.nn.bias_add(conv, biases)
+            conv5_2 = tf.nn.relu(out, name=scope)
+            self._parameters += [kernel, biases]
+
+        # conv5_3
+        with tf.name_scope('conv5_3') as scope:
+            kernel = tf.Variable(tf.truncated_normal([3, 3, 512, 512], dtype=tf.float32, stddev=1e-1), name='weights')
+            conv = tf.nn.conv2d(conv5_2, kernel, [1, 1, 1, 1], padding='SAME')
+            biases = tf.Variable(tf.constant(0.0, shape=[512], dtype=tf.float32), trainable=True, name='biases')
+            out = tf.nn.bias_add(conv, biases)
+            conv5_3 = tf.nn.relu(out, name=scope)
+            self._parameters += [kernel, biases]
+
+        # pool5
+        self._pool5 = tf.nn.max_pool(conv5_3,
+                               ksize=[1, 2, 2, 1],
+                               strides=[1, 2, 2, 1],
+                               padding='SAME',
+                               name='pool5')
+
+    def _create_vgg16_fc_conv_layers(self):
+        # here we create first two FC layers of VGG16, but as 1x1 convolutions
+
+        # fc1 -> conv6
+        with tf.name_scope('conv6') as scope:
+            kernel = tf.Variable(tf.truncated_normal([1, 1, 512, 4096], dtype=tf.float32, stddev=1e-1), name='weights')
+            conv = tf.nn.conv2d(self._pool5, kernel, [1, 1, 1, 1], padding='SAME')
+            biases = tf.Variable(tf.constant(0.0, shape=[4096], dtype=tf.float32), trainable=True, name='biases')
+            out = tf.nn.bias_add(conv, biases)
+            relu = tf.nn.relu(out, name='relu')
+            conv6 = tf.nn.dropout(relu, self._keep_prob, name=scope)
+            self._parameters += [kernel, biases]
+
+        # fc2 -> conv7
+        with tf.name_scope('conv7') as scope:
+            kernel = tf.Variable(tf.truncated_normal([1, 1, 4096, 4096], dtype=tf.float32, stddev=1e-1), name='weights')
+            conv = tf.nn.conv2d(conv6, kernel, [1, 1, 1, 1], padding='SAME')
+            biases = tf.Variable(tf.constant(0.0, shape=[4096], dtype=tf.float32), trainable=True, name='biases')
+            out = tf.nn.bias_add(conv, biases)
+            relu = tf.nn.relu(out, name='relu')
+            self._conv7 = tf.nn.dropout(relu, self._keep_prob, name=scope)
+            self._parameters += [kernel, biases]
---
+++ main2.py
@@ -0,0 +1,138 @@
+import os.path
+import tensorflow as tf
+import helper
+import warnings
+from distutils.version import LooseVersion
+import project_tests as tests
+
+from tqdm import tqdm
+import math
+
+import fcn8vgg16
+
+"""Semantic Segmentation with Fully Convolutional Networks
+
+based on https://people.eecs.berkeley.edu/~jonlong/long_shelhamer_fcn.pdf
+"""
+
+_n_samples = 2975
+_keep_probability_value = 0.9
+_learning_rate_value = 0.0001
+_gpu_count = 0
+_gpu_mem_fraction = 0.9
+_epochs = 1
+_batch_size = 5
+
+# Check TensorFlow Version
+assert LooseVersion(tf.__version__) >= LooseVersion('1.0'), \
+    'Please use TensorFlow version 1.0 or newer.  You are using {}'.format(tf.__version__)
+print('TensorFlow Version: {}'.format(tf.__version__))
+
+# Check for a GPU
+if not tf.test.gpu_device_name():
+    warnings.warn('No GPU found. Please use a GPU to train your neural network.')
+else:
+    print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
+
+def load_trained_vgg_vars(sess, vgg_path):
+    """
+    Load Pretrained VGG Model into TensorFlow.
+    :param sess: TensorFlow Session
+    :param vgg_path: Path to vgg folder, containing "variables/" and "saved_model.pb"
+    :return: Tuple of Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
+    """
+    vgg_tag = 'vgg16'
+    tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
+    graph = tf.get_default_graph()
+
+    all_ops = graph.get_operations()
+    vars = [op for op in all_ops if op.op_def and op.op_def.name[:5] == 'Varia']
+
+    return vars
+
+
+def run():
+    # cityscapes
+    #  https://www.cityscapes-dataset.com/
+    num_classes = 35
+    image_shape = (256, 512)
+    data_dir = '../cityscapes/data'
+    runs_dir = './runs_city'
+    # Create function to get batches
+    get_batches_fn = helper.gen_batch_function_cityscapes(os.path.join(data_dir, ''), image_shape)
+    test_data_dir = os.path.join(data_dir, 'leftImg8bit/test/*/*.png')
+
+    # Download pretrained vgg model
+    model_dir = './data'
+    helper.maybe_download_pretrained_vgg(model_dir)
+
+    config = tf.ConfigProto(log_device_placement=False, device_count = {'GPU': _gpu_count})
+    config.gpu_options.allow_growth = True
+    config.gpu_options.per_process_gpu_memory_fraction = _gpu_mem_fraction
+
+    # load pre-trained VGG weights
+    with tf.Session(config=config) as sess:
+        vgg_path = os.path.join(model_dir, 'vgg')
+        vars = load_trained_vgg_vars(sess, vgg_path)
+        var_values = {}
+        for var in vars:
+            name = var.name
+            tensor = tf.get_default_graph().get_tensor_by_name(name+':0')
+            value = sess.run(tensor)
+            name = name.replace('filter','weights')
+            name = name.replace('fc','conv')
+            var_values[name] = value
+
+    with tf.Session(config=config) as sess:
+        # define our FCN
+        images_shape = (None,)+image_shape+(3,)
+        labels_shape = (None,)+image_shape+(num_classes,)
+        model = fcn8vgg16.FCN8_VGG16(images_shape, labels_shape)
+        # run default initialisers
+        sess.run(tf.global_variables_initializer())
+
+        # restore trained weights for VGG
+        for var in model._parameters:
+            name = var.name.replace('encoder_vgg16/', '').replace(':0','')
+            value = var_values[name]
+            tf.assign(var, value)
+
+        # TODO: Augment Images for better results
+        #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
+
+        final_loss = model.train(sess, _epochs, _batch_size, get_batches_fn,
+                                 5,#_n_samples,
+                                 _keep_probability_value, _learning_rate_value)
+
+        return
+
+        # Save inference data using helper.save_inference_samples
+        output_dir = helper.save_inference_samples(runs_dir, test_data_dir, sess, image_shape, logits, keep_prob, image_input)
+        with open(os.path.join(output_dir, "params.txt"), "w") as f:
+            f.write('keep_prob={}\n'.format(_keep_probability_value))
+            f.write('data_dir={}\n'.format(data_dir))
+            f.write('n_samples={}\n'.format(_n_samples))
+            f.write('batch={}\n'.format(_batch_size))
+            f.write('epochs={}\n'.format(_epochs))
+            f.write('use_gpu={}\n'.format(_gpu_count))
+            f.write('gpu_mem={}\n'.format(_gpu_mem_fraction))
+            f.write('lr={}\n'.format(_learning_rate_value))
+            f.write('n_samples={}\n'.format(_n_samples))
+            f.write('final_loss={}\n'.format(final_loss))
+            #f.write('n_samples={}'.format(_))
+
+        # save model
+        """ save trained model using SavedModelBuilder """
+        model_dir = os.path.join(output_dir, 'model')
+        print('saving SavedModel into {}'.format(model_dir))
+        builder = tf.saved_model.builder.SavedModelBuilder(model_dir)
+        tag = 'FCN8'
+        builder.add_meta_graph_and_variables(sess, [tag])
+        builder.save()
+
+
+                # TODO: Apply the trained model to a video
+
+
+if __name__ == '__main__':
+    run()
