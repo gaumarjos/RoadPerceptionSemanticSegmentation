e09e8577dc4bcbc01a998e705d06bdc6bc450637
Alexey Simonov <alexey.simonov@gmail.com>
2017-09-24 17:36:20 +0100

    empirically found graph transforms that work

--- optimise.sh
+++ optimise.sh
@@ -1,6 +1,16 @@
 # first parameter $1 frozen_model_dir
 
-#remove_nodes(op=Identity, op=CheckNumerics)
+# ideal transforms, but some of them break inference
+#+add_default_attributes
+#- remove_nodes(op=Identity, op=CheckNumerics)
+#+fold_constants(ignore_errors=true)
+#+fold_batch_norms
+#+fold_old_batch_norms
+#+fuse_resize_and_conv
+#+quantize_weights increase time from 18ms to 50ms. quality seems ok
+#- quantize_nodes
+#+strip_unused_nodes
+#+sort_by_execution_order
 
 ~/dev/tf/tensorflow-r1.3/bazel-bin/tensorflow/tools/graph_transforms/transform_graph \
 --in_graph=$1/graph.pb \
@@ -14,6 +24,6 @@ fold_batch_norms
 fold_old_batch_norms
 fuse_resize_and_conv
 quantize_weights
-quantize_nodes
 strip_unused_nodes
 sort_by_execution_order'
+
