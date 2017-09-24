9f763003074c3468401cbbe4ef5b56b75891f63a
Alexey <alexey.simonov@gmail.com>
2017-09-24 16:45:16 +0100

    removing identity from graph_transform as we have map_fn for preprocessing

--- optimise.sh
+++ optimise.sh
@@ -1,5 +1,7 @@
 # first parameter $1 frozen_model_dir
 
+#remove_nodes(op=Identity, op=CheckNumerics)
+
 ~/dev/tf/tensorflow-r1.3/bazel-bin/tensorflow/tools/graph_transforms/transform_graph \
 --in_graph=$1/graph.pb \
 --out_graph=$1/optimised_graph.pb \
@@ -7,7 +9,6 @@
 --outputs=predictions/prediction_class \
 --transforms='
 add_default_attributes
-remove_nodes(op=Identity, op=CheckNumerics)
 fold_constants(ignore_errors=true)
 fold_batch_norms
 fold_old_batch_norms
