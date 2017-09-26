f93735450e31daeb0649d0fe760f13f6a29c6393
Alexey <alexey.simonov@gmail.com>
2017-09-26 23:16:21 +0100

    creating optimized dir

--- main.py
+++ main.py
@@ -341,8 +341,8 @@ def optimise_graph(args):
 
     # save model in same format as usual
     shutil.rmtree(args.optimised_model_dir, ignore_errors=True)
-    if not os.path.exists(args.optimised_model_dir):
-        os.makedirs(args.optimised_model_dir)
+    #if not os.path.exists(args.optimised_model_dir):
+    #    os.makedirs(args.optimised_model_dir)
 
     print('saving optimised model as saved_model to {}'.format(args.optimised_model_dir))
     model = fcn8vgg16.FCN8_VGG16(define_graph=False)
