4ce70beb5fc877c6b279acd9974a7fbc89420e8c
Alexey <alexey.simonov@gmail.com>
2017-09-27 13:20:04 +0100

    adding GPU use for video prediction

--- 05_video.sh
+++ 05_video.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-python main.py video --model_dir=optimised_model --video_file_in=project_video.mp4 --video_file_out=segmented.mp4
-
+python main.py video --gpu=1 --xla=2 --model_dir=optimised_model --video_file_in=project_video.mp4 --video_file_out=segmented.mp4
