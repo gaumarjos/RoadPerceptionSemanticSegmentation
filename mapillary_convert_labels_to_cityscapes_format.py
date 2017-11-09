b315c5f1dd4c59b367fbfaced5056f9b301e4f1c
Stefano Salati <stef.salati@gmail.com>
2017-11-09 16:56:01 +0100

    Modified to work directly with instance images and different folders

--- mapillary_convert_labels_to_cityscapes_format.py
+++ mapillary_convert_labels_to_cityscapes_format.py
@@ -15,18 +15,7 @@ import cv2
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from pylab import *              # Matplotlib's pylab interface
-ion()                            # Turned on Matplotlib's interactive mode
 from collections import deque
-
-images_path_pattern = '../mapillary/data/train/*.jpg'
-labels_path_pattern = '../mapillary/data/train/*.png'
-verbose = False
-
-desired_h = 1024
-desired_w = 2048
-desired_ratio = desired_w / desired_h
-desired_top_crop_ratio = 0.66  # 1 = crop only top, 0 = crop only bottom, 0.5 = equal
-assert desired_top_crop_ratio <= 1.0 and desired_top_crop_ratio >= 0.0
 
 
 def change_color(fromimage, toimage, fromcolor, tocolor):
@@ -38,106 +27,104 @@ def change_color(fromimage, toimage, fromcolor, tocolor):
     # toimage[:,:,:3][mask] = [r2, g2, b2]
     return np.sum(mask)
 
-"""
-def find_horizon(image, path):
-    hls = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
-    l = hls[:,:,1]
-    print(l.shape)
-    distribution = np.sum(l, axis=1)
-    print(distribution.shape)
-    print(distribution)
-    figure(1, figsize=(6, 6))
-    plt.plot(distribution)
-    plt.ylabel('Distr.')
-    plt.show()
-    savefig(path)
-    return
-"""
 
-# Find images to convert
-image_paths = glob.glob(images_path_pattern)
-label_paths = {re.sub('png', 'jpg', os.path.basename(path)): path for path in glob.glob(labels_path_pattern)}
-assert len(image_paths) == len(label_paths)
+def process_folder(images_path_pattern, labels_path_pattern):
+    # Find images to convert
+    image_paths = glob.glob(images_path_pattern)
+    label_paths = {re.sub('png', 'jpg', os.path.basename(path)): path for path in glob.glob(labels_path_pattern)}
+    assert len(image_paths) == len(label_paths)
 
-# Used to estimate the remaining time
-processing_times = deque([], maxlen=5)
+    # Used to estimate the remaining time
+    processing_times = deque([], maxlen=10)
 
-for i, image_file in enumerate(image_paths):
-    start_time = time.time()
+    for i, image_file in enumerate(image_paths):
+        start_time = time.time()
 
-    # Output filenames
-    output_image_file = os.path.splitext(image_file)[0] + '_image.png'
-    gt_image_file = label_paths[os.path.basename(image_file)]
-    output_gt_image_file = os.path.splitext(gt_image_file)[0] + '_gt.png'
-    # output_stats_file = os.path.splitext(gt_image_file)[0] + '_stats.png'
+        # Output filenames
+        output_image_file = os.path.splitext(image_file)[0] + '_cropped.png'
+        gt_image_file = label_paths[os.path.basename(image_file)]
+        output_gt_image_file = os.path.splitext(gt_image_file)[0] + '_cropped.png'
 
-    # Read images
-    image = scipy.misc.imread(image_file)
-    gt_image = scipy.misc.imread(gt_image_file)
+        # Read images
+        image = scipy.misc.imread(image_file)
+        gt_image = scipy.misc.imread(gt_image_file)
+        assert image.shape[0] == gt_image.shape[0] and image.shape[1] == gt_image.shape[1] and image.shape[2] == 3
 
-    print("({}/{}) Processing {} ({})...".format(i+1, len(image_paths), gt_image_file, gt_image.shape))
+        print("({}/{}) Processing the pair {} ({}) - {} ({})...".format(i+1, len(image_paths),
+                                                                 image_file, image.shape,
+                                                                 gt_image_file, gt_image.shape))
 
-    # Crop both image and gt to be the desired_ratio
-    h = gt_image.shape[0]
-    w = gt_image.shape[1]
-    ratio = w/h
-    # find_horizon(image, output_stats_file)
-    if ratio > desired_ratio:
-        tocrop = int(w - h * desired_ratio)
-        tocrop_left = int(0.5 * tocrop)
-        w = w - tocrop
-        #print("Need to crop horizontally, {}px per side".format(tocrop))
-        image    =    image[0:h, tocrop_left:tocrop_left+w]
-        gt_image = gt_image[0:h, tocrop_left:tocrop_left+w]
-    elif ratio < desired_ratio:
-        tocrop = int(h - w / desired_ratio)
-        tocrop_top = int(desired_top_crop_ratio * tocrop)
-        h = h - tocrop
-        #print("Need to crop vertically, {}px per side".format(tocrop))
-        image    =    image[tocrop_top:tocrop_top+h, 0:w]
-        gt_image = gt_image[tocrop_top:tocrop_top+h, 0:w]
-    else:
-        pass
-        #print("NO need to crop")
-    #print(image.shape)
-    #print(gt_image.shape)
+        # Crop both image and gt to be the desired_ratio
+        h = gt_image.shape[0]
+        w = gt_image.shape[1]
+        ratio = w/h
+        # find_horizon(image, output_stats_file)
+        if ratio > desired_ratio:
+            tocrop = int(w - h * desired_ratio)
+            tocrop_left = int(0.5 * tocrop)
+            w = w - tocrop
+            #print("Need to crop horizontally, {}px per side".format(tocrop))
+            image    =    image[0:h, tocrop_left:tocrop_left+w]
+            gt_image = gt_image[0:h, tocrop_left:tocrop_left+w]
+        elif ratio < desired_ratio:
+            tocrop = int(h - w / desired_ratio)
+            tocrop_top = int(desired_top_crop_ratio * tocrop)
+            h = h - tocrop
+            #print("Need to crop vertically, {}px per side".format(tocrop))
+            image    =    image[tocrop_top:tocrop_top+h, 0:w]
+            gt_image = gt_image[tocrop_top:tocrop_top+h, 0:w]
+        else:
+            pass
+            #print("No need to crop")
+        #print(image.shape)
+        #print(gt_image.shape)
 
-    # Resize it to be desired_h * desired_w
-    image_res = cv2.resize(image, (desired_w, desired_h), interpolation=cv2.INTER_NEAREST)
-    gt_image_res = cv2.resize(gt_image, (desired_w, desired_h), interpolation=cv2.INTER_NEAREST)
+        # Resize it to be desired_h * desired_w
+        image_res = cv2.resize(image, (desired_w, desired_h), interpolation=cv2.INTER_NEAREST)
+        gt_image_res = cv2.resize(gt_image, (desired_w, desired_h), interpolation=cv2.INTER_NEAREST)
 
-    # Make sure the color image is 8 bit and create gt_image_bw output
-    image = image.astype(dtype=np.uint8)
-    gt_image_bw = np.zeros((gt_image_res.shape[0], gt_image_res.shape[1]), dtype=np.uint8)
+        # Make sure the color image is 8 bit
+        image_res = image_res.astype(dtype=np.uint8)
+        gt_image_res = gt_image_res.astype(dtype=np.uint8)
+        
+        """
+        verbose = False
+        # Scroll through all possible labels and paint the output accordingly
+        gt_image_bw = np.zeros((gt_image_res.shape[0], gt_image_res.shape[1]), dtype=np.uint8)
+        num_classes = len(mapillary_labels.labels)
+        for c in range(num_classes):
+            fromcolor = mapillary_labels.labels[c].color_prepr
+            tocolor = mapillary_labels.labels[c].trainId
+            n_changed_px = change_color(gt_image_res, gt_image_bw, fromcolor, tocolor)
+            if verbose:
+                print("    ({:5.2f}%)    RGB {:3}, {:3}, {:3} --> LABEL {:3}    {}".format(n_changed_px/gt_image_bw.size*100,
+                                                                                    fromcolor[0], fromcolor[1], fromcolor[2],
+                                                                                    tocolor,
+                                                                                    mapillary_labels.labels[i].name))
+        """
 
-    # Scroll through all possible labels and paint the output accordingly
-    num_classes = len(mapillary_labels.labels)
-    for c in range(num_classes):
-        fromcolor = mapillary_labels.labels[c].color_prepr
-        tocolor = mapillary_labels.labels[c].trainId
-        n_changed_px = change_color(gt_image_res, gt_image_bw, fromcolor, tocolor)
-        if verbose:
-            print("    ({:5.2f}%)    RGB {:3}, {:3}, {:3} --> LABEL {:3}    {}".format(n_changed_px/gt_image_bw.size*100,
-                                                                                fromcolor[0], fromcolor[1], fromcolor[2],
-                                                                                tocolor,
-                                                                                mapillary_labels.labels[i].name))
+        # Save both the resized image and the gt image with B/W labelling
+        scipy.misc.imsave(output_image_file, image_res)
+        scipy.misc.imsave(output_gt_image_file, gt_image_res)
 
-    # Save both the resized image and the gt image with B/W labelling
-    scipy.misc.imsave(output_image_file, image_res)
-    scipy.misc.imsave(output_gt_image_file, gt_image_bw)
+        # Compute processing duration
+        duration = time.time() - start_time
+        processing_times.append(duration)
+        avg_duration = np.mean(np.asarray(processing_times))
+        remaining_time = int((len(image_paths) - (i+1)) * avg_duration)
+        remaining_time_m, remaining_time_s = divmod(remaining_time, 60)
+        remaining_time_h, remaining_time_m = divmod(remaining_time_m, 60)
+        print("Remaining time: {:02d}:{:02d}:{:02d}".format(remaining_time_h, remaining_time_m, remaining_time_s))
 
-    # Just check
-    """
-    check = scipy.misc.imread(output_gt_image_file)
-    assert np.max(gt_image_bw) == np.max(check)
-    """
 
-    # Compute processing duration
-    duration = time.time() - start_time
-    processing_times.append(duration)
-    avg_duration = np.mean(np.asarray(processing_times))
-    remaining_time = int((len(image_paths) - (i+1)) * avg_duration)
-    remaining_time_m, remaining_time_s = divmod(remaining_time, 60)
-    remaining_time_h, remaining_time_m = divmod(remaining_time_m, 60)
-    print("Remaining time: {:02d}:{:02d}:{:02d}".format(remaining_time_h, remaining_time_m, remaining_time_s))
+if __name__ == '__main__':
+    desired_h = 1024
+    desired_w = 2048
+    desired_ratio = desired_w / desired_h
+    desired_top_crop_ratio = 0.66  # 1 = crop only top, 0 = crop only bottom, 0.5 = equal
+    assert desired_top_crop_ratio <= 1.0 and desired_top_crop_ratio >= 0.0
+
+    images_path_pattern = '../mapillary/data/training/images/*.jpg'
+    labels_path_pattern = '../mapillary/data/training/instances/*.png'
+    process_folder(images_path_pattern, labels_path_pattern)
 
--- mapillary_run_conversion.sh
+++ mapillary_run_conversion.sh
@@ -1,5 +1,3 @@
-rm ../mapillary/data/train/*_gt.png
-rm ../mapillary/data/train/*_image.png
-rm ../mapillary/data/train/*_stats.png
-python mapillary_convert_labels_to_cityscapes_format.py 
-
+rm ../mapillary/data/training/images/*_cropped.png
+rm ../mapillary/data/training/instances/*_cropped.png
+python mapillary_convert_labels_to_cityscapes_format.py
