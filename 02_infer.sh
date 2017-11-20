26024889a1630f1c0f7081eed9ed08d791e1c3be
Stefano Salati <stef.salati@gmail.com>
2017-11-20 14:13:59 +0100

    Script changes

--- 02_infer.sh
+++ 02_infer.sh
@@ -21,22 +21,32 @@
 
 # Urban Cologne videos
 # TRAINING="Training_A"
-TRAINING=""
-IMAGES=1
-VIDEO=0
+TRAINING="mapillary_training_b"
+IMAGES=0
+VIDEO=1
+OPTIMISE=0
 
 MASTERVIDEOFOLDER="20171103_DrivePX_City"
-VIDEOFOLDER="dw_20171102_181952_0.000000_0.000000"
-# VIDEONAME="video_1.h264_undist"
-VIDEONAME="video_4.h264"
+:'
+VIDEOFOLDERS=( "dw_20171102_182846_0.000000_0.000000"     # Aachener Str. direction centre
+               "dw_20171102_185404_0.000000_0.000000"     # Crossing cyclist
+               "dw_20171102_185840_0.000000_0.000000" )   # Small neighbourhood
+'
+VIDEOFOLDERS=( "dw_20171102_180413_0.000000_0.000000"     # Durener
+               "dw_20171102_181333_0.000000_0.000000"     # Small neighbourhood
+               "dw_20171102_183737_0.000000_0.000000")    # Deutz bridge
+VIDEONAME="video_4.h264_256x512"
 
 EXTENSION="mp4"
-SUFFIX="_segmented"
+SUFFIX="_segmented_mapillary_b"
 VIDEOFILEINPUT=$VIDEONAME.$EXTENSION
 VIDEOFILEOUTPUT=$VIDEONAME$SUFFIX.$EXTENSION
 
-python main.py freeze --ckpt_dir=ckpt/$TRAINING --frozen_model_dir=frozen_model
-python main.py optimise --frozen_model_dir=frozen_model --optimised_model_dir=optimised_model
+if [ $OPTIMISE -eq 1 ]
+then
+  python main.py freeze --ckpt_dir=ckpt/$TRAINING --frozen_model_dir=frozen_model
+  python main.py optimise --frozen_model_dir=frozen_model --optimised_model_dir=optimised_model
+fi
 
 if [ $IMAGES -eq 1 ]
 then
@@ -46,7 +56,12 @@ fi
 
 if [ $VIDEO -eq 1 ]
 then
-  echo "Processing $VIDEOFILEINPUT --> $VIDEOFILEOUTPUT"
-  python main.py video --gpu=1 --xla=2 --model_dir=optimised_model --video_file_in=../videos/$MASTERVIDEOFOLDER/$VIDEOFOLDER/$VIDEOFILEINPUT --video_file_out=../videos/$MASTERVIDEOFOLDER/$VIDEOFOLDER/$VIDEOFILEOUTPUT
+  for VIDEOFOLDER in "${VIDEOFOLDERS[@]}"
+  do
+    echo "[$VIDEOFOLDER] processing $VIDEOFILEINPUT --> $VIDEOFILEOUTPUT"
+    python main.py video --gpu=1 --xla=2 --model_dir=optimised_model --video_file_in=../videos/$MASTERVIDEOFOLDER/$VIDEOFOLDER/$VIDEOFILEINPUT --video_file_out=../videos/$MASTERVIDEOFOLDER/$VIDEOFOLDER/$VIDEOFILEOUTPUT
+    # --video_start_second 0 --video_end_second 60
+  done
 fi
 
+#
